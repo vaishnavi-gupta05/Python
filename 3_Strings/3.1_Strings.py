fe04fbeb073bf602116e5812ce1555eb23b82c35
Vaishnavi Gupta <vaishnavigu2005@gmail.com>
2026-07-14 18:06:02 +0530

    Add 3_Strings\3.1_Strings.py

---
+++ 3_Strings/3.1_Strings.py
@@ -0,0 +1,43 @@
+# # STRINGS
+# # String is a sequence of characters. It is a data type in Python that is used to represent text. Strings are enclosed in either single quotes (' ') or double quotes (" ").
+
+# str_1= 'Vaishnavi'
+# print(str_1)
+
+# str_2= "Vaishnavi"
+# print(str_2)
+
+# str_3= '''first line
+# this is second line with tripple quotes in string
+# this is the third'''
+# print(str_3)
+
+# # INDEXING
+# # Indexing is used to access individual characters of a string using their position number (index).
+
+# indexing_ = "Vaishnavi Gupta"
+# #            0123456789         ---positive indexing
+# #                          -1   ---negetive indexing
+# print(indexing_[0])
+# print(indexing_[1])
+# print(indexing_[2])
+# print(indexing_[3])
+# print(indexing_[4])
+# print(indexing_[9]) #space also occupy index
+
+# # negative indexing
+# # -1 is the last character in the string
+
+# print(indexing_[-1])
+# print(indexing_[-2])
+# print(indexing_[-3])
+
+# # to convert -ve index  into +ve index -- you can add the size od the string 
+
+# print(indexing_[-1+ len(indexing_)])
+
+
+
+
+
+
