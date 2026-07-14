8cdb4ff51997d58c419b580c7cda38a8bdc97d33
Vaishnavi Gupta <vaishnavigu2005@gmail.com>
2026-07-14 18:07:47 +0530

    Add 3_Strings\3.2_Slicing_Indexing.py

---
+++ 3_Strings/3.2_Slicing_Indexing.py
@@ -0,0 +1,25 @@
+# # Slicing in strings
+
+# slicing_= "Vaishnavi"
+# print(slicing_[0:9]) # will print index zero to index 9-1
+
+# print(slicing_[0:-1])
+# # is same as [0: -1+ size of string]---[0:8]
+
+# print(slicing_[0:-2])
+# # [0:7]
+
+# #step slicing: skinping characters
+# # slicing_[0:9:n] --> skip n-1 characters
+
+# print(slicing_[0:9:2]) #skip 1 character
+
+# # blank case
+# print(slicing_[:])
+# #replace first empty with 0 and second empty with string size
+# print(slicing_[0:])
+# print(slicing_[:9])
+
+
+
+
